ec126cd6d571eb6af7420bfd5b481ce1382d3d88
jmalvarez97 <jualvarez@fi.uba.ar>
2023-01-08 11:51:06 -0300

    cambios en contact, mvp

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,6 +2,12 @@ import styles from "../styles/main.module.css"
 import { ReactComponent as Decoration } from '../static/images/decoration-star.svg';
 import { useRef, useEffect } from "react";
 
+import {BsInstagram} from "react-icons/bs" 
+import {AiOutlineWhatsApp} from "react-icons/ai"
+import chanchito from "../static/images/ch.png";
+
+
+import 'mapbox-gl/dist/mapbox-gl.css'; 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam1hbHYiLCJhIjoiY2xjbHh5eTNlMW02YTNwcDh6eHAwcW12MiJ9.16H6PbPrliW6jrtAWDmHHw';
 
@@ -21,21 +27,36 @@ const Contact = () => {
                 />
                 <div className={`${styles.container} container`}>
                     <div className="row">
-                        <div className={`col-lg-6 ${styles.col}`}>
+                        <div className={`col-lg-6 ${styles.mapColumn}`}>
                             <div className={`${styles.imageContainer}`}>
+
+                            <h2>Donde nos ubicamos?</h2>
+
+                            <p>Sector Industrial II - Alberti - Pcia. de Buenos Aires</p>
 
 
                             {useEffect(() => {
                                 if (map.current) return; // initialize map only once
+
+                                
                                 map.current = new mapboxgl.Map({
                                     container: mapContainer.current,
                                     style: 'mapbox://styles/mapbox/streets-v12',
-                                    center: [-60.2888, -35.0324],
-                                    zoom: 13.79
+                                    center: [-60.2890, -35.0430],
+                                    zoom: 12.20
                                 });
-                                new mapboxgl.Marker()
-                                            .setLngLat([-60.2888, -35.0324])
+
+                                var popup = new mapboxgl.Popup()
+                                            .setText(`Chacinados David`)
                                             .addTo(map.current);
+
+                                new mapboxgl.Marker({
+                                    draggable : false,
+                                    color : "#0c9d7c"
+                                })
+                                            .setLngLat([-60.29572312809677,-35.03088234658322])
+                                            .addTo(map.current)
+                                            .setPopup(popup);
                             })}
                                         
                             </div> 
@@ -43,25 +64,41 @@ const Contact = () => {
                         </div> 
                         <div className={`${styles.col} col-lg-6`}>
                             <div className={`${styles.textContainer}`}>
-                                <h2>Contact us for a quote using the following form</h2>
-
-                            
+                                <h2>Contactanos para la venta de nuestros productos</h2>
                                 <form>
                                     <div className={`${styles.formGroup}`}>
-                                        <input type="text" className={`${styles.formControlInput}`} placeholder="Name" required></input>
+                                        <input type="text" className={`${styles.formControlInput}`} placeholder="Nombre" required></input>
                                     </div>
                                     <div className={`${styles.formGroup}`}>
-                                        <input type="email" className={`${styles.formControlInput}`} placeholder="Email" required></input>
+                                        <input type="text" className={`${styles.formControlInput}`} placeholder="Telefono" required></input>
                                     </div>
                                     <div className={`${styles.formGroup}`}>
-                                        <textarea className={`${styles.formControlTextArea}`} placeholder="Message" required></textarea>
+                                        <input type="email" className={`${styles.formControlInput}`} placeholder="E-mail (opcional)"></input>
                                     </div>
                                     <div className={`${styles.formGroup}`}>
-                                        <button type="submit" className={`${styles.formControlSubmit}`}>Submit</button>
+                                        <textarea className={`${styles.formControlTextArea}`} placeholder="Mensaje"></textarea>
+                                    </div>
+                                    <div className={`${styles.formGroup}`}>
+                                        <button type="submit" className={`${styles.formControlSubmit}`}>Enviar</button>
                                     </div>
                                 </form>
+                                <h2 className={styles.textContainer}>Nuestras Redes</h2>
+                                <div>
+                                    <a href="https://www.instagram.com/chacinadosdavid/" className={styles.networkUl}>
+                                        <BsInstagram className={styles.contactIcons}/>
+                                        <p className={styles.networkP}>chacinadosdavid</p>
+                                    </a>
+                                </div>
+                                <div className={styles.networkUl}>
+                                    <AiOutlineWhatsApp className={styles.contactIcons}/>
+                                    <p className={styles.networkP}>+549 2346 555080</p>
+                                </div>
                                 
-                            </div> 
+                                
+                            </div>                            
+                        </div> 
+                        <div className={`image-container ${styles.imageContainer}`}>
+                                <img className={`img-fluid ${styles.piggyContact}`} src={chanchito} alt="alternative"></img>
                         </div> 
                     </div> 
                 </div> 
